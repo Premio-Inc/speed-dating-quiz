1ef538eadcf2bf196a35897cb7189a2d699269d5
Charles Chau <chausieu@gmail.com>
2025-06-17 17:26:54 -0700

    no jekyll

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const repoName = "speed-dating-quiz";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   trailingSlash: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
